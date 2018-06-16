67fc991f8ed0ddb8a2e629383a10deeb30c663ea
local <localhost@local.com>
2018-06-16 11:53:03 -0400

    committed

--- Hello.java
+++ Hello.java
@@ -1,8 +1,15 @@
 import java.io.*;
+import java.util.*;
 public class Hello
 {
 public static void main(String args[])
 {
+int a,b,c;
+Scanner Sc=new Scanner(System.in);
+a=Sc.nextInt();
+b=Sc.nextInt();
+c=a+b;
+System.out.println(c+"total");
 for(int i=0;i<10;i++)
 System.out.println("Hello GitHub");
 }
